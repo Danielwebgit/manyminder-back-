867516e0aa859977b73c6e370804db8450deb19e
danielwebgit <nielferas@gmail.com>
2023-04-26 11:47:35 -0300

    apenas removido linhas nao usadas

--- application/middlewares/AccessControlMiddleware.php
+++ application/middlewares/AccessControlMiddleware.php
@@ -2,10 +2,7 @@
 
 require_once APPPATH . 'third_party/php-jwt/JWT.php';
 require_once APPPATH . 'middlewares/Abstract/AbstractAuthorization.php';
-
-use Firebase\JWT\JWT;
 use Abastract\AbstractAuthorization;
-
 class AccessControlMiddleware extends AbstractAuthorization {
     
     protected $controller;
@@ -24,35 +21,10 @@ class AccessControlMiddleware extends AbstractAuthorization {
 
     public function run()
     {
-        //$auth_header = $this->CI->input->get_request_header('Authorization', FALSE);
-       
         $headers = $this->CI->input->request_headers();
-
-        
-
         $token = str_replace('Bearer ', '', $headers['Authorization']);
-        
-        // if(isset($headers['Authorization']) == true) {
-            
-        //     $token = str_replace('Bearer ', '', $headers['Authorization']);
-        // } else {
-        //     response_helper('message', 'Token ausente no cabeçalho!', 401);
-        //     die;
-        // }
-
-        
-        
-        
         $decodedToken = $this->validateToken($token);
-       
-        
-        //die(response_helper('error', $decodedToken));
-        //response_helper('message', $decodedToken, 401);
-        //$payload = JWT::decode($headers['Authorization'], 'eyJ0eXAiOiJKV1QiLCJhbGciTWvLUzI1NiJ9IiRkYXRhIg', array('HS256'));
-     
         $route = $this->CI->router->class.'/'.$this->CI->router->method;
-        //$user_id = $this->CI->session->userdata('user_id');
-        
         $rules_permissions = $this->CI->Rules_permissions_model->index_rules_permissions($decodedToken->id);
         
         $permissions = [];
@@ -66,6 +38,5 @@ class AccessControlMiddleware extends AbstractAuthorization {
         if(! $isAllowed){
             die(response_helper('error', 'Access denied'));
         }
-            
     }
 }
